16febba6217a2458deba8038e8e5b23bf28cc4da
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-11-22 21:04:08 +0300

    make dashboard functional for admin page

--- admin/index.php
+++ admin/index.php
@@ -15,24 +15,45 @@
         <h1>Dashboard</h1>
 
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php 
+                $sql = "SELECT * FROM tbl_category";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+            ?>
+            <h1><?php echo $count; ?></h1>
             <br>
             Categories
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php 
+                $sql1 = "SELECT * FROM tbl_food";
+                $res1 = mysqli_query($conn, $sql1);
+                $count1 = mysqli_num_rows($res1);
+             ?>
+            <h1><?php echo $count1; ?></h1>
             <br>
-            Categories
+            Foods
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php $sql2 = "SELECT * FROM tbl_order";
+                $res2 = mysqli_query($conn, $sql2);
+                $count2 = mysqli_num_rows($res2);
+            ?>
+            <h1><?php echo $count2; ?></h1>
             <br>
-            Categories
+            Total Order
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php 
+              $sql3 = "SELECT SUM(total) AS TotalRevenue FROM tbl_order";
+              $res3 = mysqli_query($conn, $sql3);
+              $row = mysqli_fetch_assoc($conn, $res3);
+
+              $total_revenue = $row['TotalRevenue'];
+            ?>
+            <h1><?php echo $total_revenue; ?></h1>
             <br>
-            Categories
+            Revenue Generated
         </div>
 
         <div class="clearfix"></div>
